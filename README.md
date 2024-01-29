8c1785e639a66fef64b9bd7680b46387f4da6eb5
Joseph Sirianni <joe.sirianni@observiq.com>
2024-01-29 11:33:07 -0500

    feat: Initial Action (#2)
    * wip
    
    * fix matrix
    
    * install cli
    
    * pull image
    
    * run bindplane in background
    
    * arg syntax
    
    * fix action name and uses field
    
    * switch to docker
    
    * cURL
    
    * put bindplane binary in path
    
    * fix args and add shellcheck to ci
    
    * remove missing function call
    
    * Add debug resource get
    
    * fix paths
    
    * remove dir support
    
    * fix resources for tests
    
    * wait for bindplane to become ready
    
    * try setting bindplane host
    
    * detect runner ip
    
    * detect internal ip
    
    * debug
    
    * use first addr
    
    * exec into container
    
    * test additional version
    
    * try write back without commit
    
    * test write back
    
    * ci
    
    * test write back
    
    * test write back
    
    * test write back
    
    * try write back
    
    * add github auth
    
    * BindPlane OP Action: Update OTEL Configs
    
    * clone repo
    
    * try again
    
    * latest
    
    * BindPlane OP Action: Update OTEL Configs
    
    * shellcheck
    
    * prevent out of order commits
    
    * BindPlane OP Action: Update OTEL Configs
    
    * shellcheck
    
    * return early if nothing to commit
    
    * fix out path
    
    * clone repo outside of orig context
    
    * BindPlane OP Action: Update OTEL Configs
    
    * BindPlane OP Action: Update OTEL Configs
    
    * comment
    
    * always run
    
    * fix workflow example
    
    * fix rm
    
    * skip apply if not on target branch
    
    * update org
    
    * rename params
    
    * fix bad rename
    
    * enable bool for write back. Move branch check outside of write back because it always matters
    
    * some cleanup
    
    * readme
    
    * badge
    
    * readme job name
    
    * grab pending version
    
    * add logging useful for debugging running action
    
    * remove unused files
    
    * use latest instead of pending
    
    * cleanup zip output
    
    * feat: Auto rollout (#4)
    
    * wip: auto rollout
    
    * test against this branch
    
    * add auto rollout to workflow
    
    * print config apply output
    
    * check rollout status
    
    * check for pending rollout
    
    * shellcheck: Fix loop logic
    
    * install jq
    
    * add enable_auto_rollout to readme
    
    * revert this change, use external repo for testing this pr
    
    * check for stable configs as well
    
    * remove branch from ci, we will test using an external repo
    
    * use case
    
    ---------
    
    Co-authored-by: bindplane-op-action <bindplane-op-action>

--- README.md
+++ README.md
@@ -1,1 +1,95 @@
+[![CI](https://github.com/observIQ/bindplane-op-action/actions/workflows/ci.yml/badge.svg)](https://github.com/observIQ/bindplane-op-action/actions/workflows/ci.yml)
+
 # bindplane-op-action
+
+The BindPlane OP action can be used to deploy configurations to your BindPlane OP
+server. It also supports exporting the OpenTelemetry configurations back to the repository.
+
+## Configuration
+
+| Parameter                     | Default    | Description                     |
+| :---------------------------- | :--------- | :------------------------------ |
+| bindplane_remote_url          | required   | The endpoint that will be used to connect to BindPalne OP. |
+| bindplane_api_key             |            | API key used to authenticate to BindPlane. Required when BindPlane multi account is enabled or when running on BindPlane Cloud |
+| bindplane_username            |            | Username used to authenticate to BindPlane. Not required if API key is set. |
+| bindplane_password            |            | Password used to authenticate to BindPlane.
+| target_branch                 | required   | The branch that the action will use when applying resources to bindplane or when writing otel configs back to the repo. |
+| destination_path              |            | Path to the file which contains the BindPlane destination resources |
+| configuration_path            |            | Path to the file which contains the BindPlane configuration resources |
+| enable_otel_config_write_back | `false`    | Whether or not the action should write the raw OpenTelemetry configurations back to the repository. | 
+| configuration_output_dir      |            | When write back is enabled, this is the path that will be written to. |
+| token                         |            | The Github token that will be used to write to the repo. Usually secrets.GITHUB_TOKEN is sufficient. Requires the `contents.write` permission. |
+| enable_auto_rollout           | `false`    | When enabled, the action will trigger a rollout for any configuration that has been updated. |
+
+## Usage
+
+### Export Resources
+
+To get started, you must handle exporting your existing resources to the repository. Use
+the `bindplane get` commands with the `--export` flag.
+
+```bash
+bindplane get destination -o yaml --export > destination.yaml
+bindplane get configuration -o yaml --export > configuration.yaml
+```
+
+With the resources exported to the repository, you can move on to configuring the action
+using a new workflow.
+
+### Workflow
+
+The following workflow can be used as an example. It uses the same file paths
+created in the [Export Resources](#export-resources) section.
+
+This example will write the raw OTEL configurations back to the repository at the
+path `otel/`
+
+```yaml
+name: bindplane
+
+on:
+  push:
+    branches:
+      - main
+
+# Write back requires access to the repo
+permissions:
+  contents: write
+
+# Run commits in order to prevent out of order write back commits.
+concurrency:
+  group: ${{ github.head_ref || github.ref_name }}
+  cancel-in-progress: false
+
+jobs:
+  bindplane:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Checkout
+        uses: actions/checkout@v4
+        with:
+          fetch-depth: 0
+
+      - uses: observIQ/bindplane-op-action@main
+        with:
+          bindplane_remote_url: ${{ secrets.BINDPLANE_REMOTE_URL }}
+          bindplane_username: ${{ secrets.BINDPLANE_USERNAME }}
+          bindplane_password: ${{ secrets.BINDPLANE_PASSWORD }}
+          target_branch: main
+          destination_path: destination.yaml
+          configuration_path: configuration.yaml
+          enable_otel_config_write_back: true
+          configuration_output_dir: otel/
+          token: ${{ secrets.GITHUB_TOKEN }}
+          enable_auto_rollout: true
+```
+
+After the action is executed, you can expect to see OTEL configurations
+in the `otel/` directory.
+
+```
+otel
+├── k8s-cluster.yaml
+├── k8s-gateway.yaml
+└── k8s-node.yaml
+```
---
+++ entrypoint.sh
@@ -0,0 +1,195 @@
+#!/usr/bin/env bash
+
+set -e
+
+# This order must match the order in acitons.yml
+bindplane_remote_url=${1}
+bindplane_api_key=${2}
+bindplane_username=${3}
+bindplane_password=${4}
+target_branch=${5}
+destination_path=${6}
+configuration_path=${7}
+enable_otel_config_write_back=${8}
+configuration_output_dir=${9}
+token=${10}
+enable_auto_rollout=${11}
+
+# This branch name will be compared to target_branch to determine if the action
+# should apply or write back configurations.
+BRANCH_NAME=${GITHUB_REF#refs/heads/}
+echo "Current branch is $BRANCH_NAME"
+
+install_bindplane_cli() {
+  curl -Ls \
+    -o bindplane.zip \
+    https://storage.googleapis.com/bindplane-op-releases/bindplane/latest/bindplane-ee-linux-amd64.zip
+
+  mkdir -p ~/bin
+  export PATH=$PATH:~/bin
+
+  unzip bindplane.zip -d ~/bin > /dev/null
+
+  bindplane --help > /dev/null
+}
+
+# Validate will ensure that all required variables are set
+# and generates the bindplane profile.
+validate() {
+  profile_args=""
+
+  # Target branch is always required. When not set, the script will not
+  # know which branch it should apply configurations from or write back
+  # raw otel configs.
+  if [ -z "$target_branch" ]; then
+    echo "target_branch is required when enable_otel_config_write_back is true."
+    exit 1
+  fi
+
+  if [ -z "$bindplane_remote_url" ]; then
+    echo "bindplane_remote_url is not set."
+    exit 1
+  else 
+    profile_args="$profile_args --remote-url $bindplane_remote_url"
+  fi
+
+  if [ -n "$bindplane_username" ] && [ -z "$bindplane_password" ]; then
+    echo "bindplane_password is required when bindplane_username is not set."
+    exit 1
+  else 
+    profile_args="$profile_args --username $bindplane_username --password $bindplane_password"
+  fi
+
+  if [ -z "$bindplane_username" ] && [ -z "$bindplane_api_key" ]; then
+    echo "api key is required when bindplane_username is not set."
+    exit 1
+  elif [ -n "$bindplane_api_key" ]; then
+    profile_args="$profile_args --api-key $bindplane_api_key"
+  fi
+
+  # configuration_output_dir, target_branch, and token are only required
+  # when enable_otel_config_write_back is true.
+  if [ "$enable_otel_config_write_back" = true ]; then
+    if [ -z "$configuration_output_dir" ]; then
+      echo "configuration_output_dir is required when target_branch is set."
+      exit 1
+    fi
+
+    if [ -z "$token" ]; then
+      echo "token is required when target_branch is set."
+      exit 1
+    fi
+
+    # GITHUB_ACTOR and GITHUB_REPOSITORY are set by the github actions runtime
+    # but we do expect them to be set.
+
+    if [ -z "$GITHUB_ACTOR" ]; then
+      echo "GITHUB_ACTOR is required when target_branch is set. This is likely a bug in the action. Please reach out to obserIQ support."
+      exit 1
+    fi
+
+    if [ -z "$GITHUB_REPOSITORY" ]; then
+      echo "GITHUB_REPOSITORY is required when target_branch is set. This is likely a bug in the action. Please reach out to obserIQ support."
+      exit 1
+    fi
+  fi
+
+  eval bindplane profile set "action" "$profile_args"
+  bindplane profile use "action"
+}
+
+write_back() {
+  # Clone the repo on the current branch
+  # and use depth 1 to avoid cloning the entire history.
+  git clone \
+    --depth 1 \
+    --branch "$BRANCH_NAME" \
+    "https://${GITHUB_ACTOR}:${token}@github.com/${GITHUB_REPOSITORY}.git" \
+    ../out_repo
+
+  cd "../out_repo"
+
+  mkdir -p "$configuration_output_dir"
+
+  for config in $(bindplane get config | awk 'NR>1 {print $1}'); do
+    out_file="$configuration_output_dir/$config.yaml"
+    # It is safe to always ask for "latest". BindPlane will return
+    # the current version if there is no latest version.
+    bindplane get config "${config}:latest" -o raw > "$out_file"
+    git add "$out_file"
+  done
+
+  # check if git status is clean, return early
+  if [[ -z $(git status --porcelain) ]]; then
+    echo "No changes detected. Skipping commit."
+    return
+  fi
+
+  git config --global user.email "bindplane-op-action"
+  git config --global user.name "bindplane-op-action"
+  git commit -m "BindPlane OP Action: Update OTEL Configs"
+  git push
+}
+
+main() {
+  # Short circuit if the current branch does not match the target branch,
+  # there is nothing to do.
+  if [ "$BRANCH_NAME" != "$target_branch" ]; then
+    echo "Skipping apply and repo write. Current branch ${BRANCH_NAME} does not match target branch ${target_branch}."
+    exit 0
+  fi
+
+  echo "Target branch ${target_branch} matches current branch ${BRANCH_NAME}."
+
+  # Install the CLI right away in order to construct
+  # a client profile.
+  echo "Installing bindplane CLI."
+  install_bindplane_cli
+
+  # Ensure required options are set and configure
+  # the client profile.
+  echo "Validating options and configuring client profile."
+  validate
+
+  # Apply resources in the correct order.
+  echo "Applying resources."
+
+  echo "Applying destination path: $destination_path"
+  bindplane apply "$destination_path"
+
+  echo "Applying configuration path: $configuration_path"
+  bindplane apply "$configuration_path" > configuration.out
+  cat configuration.out
+
+  # When auto rollout is enabled
+  if [ "$enable_auto_rollout" = true ]; then
+    echo "Auto rollout enabled."
+    awk '{print $2}' < configuration.out | while IFS= read -r config
+    do
+      status=$(bindplane rollout status "${config}" -o json | jq .status)
+      case "$status" in
+        0)
+          echo "Configuration ${config} has a pending rollout, triggering rollout."
+          bindplane rollout start "$config"
+          ;;
+        4)
+          echo "Configuration ${config} is stable, skipping rollout."
+          ;;
+        *)
+          echo "Configuration ${config} has an unknown status, skipping rollout."
+          ;;
+      esac
+    done
+  fi
+
+  # When write back is enabled, write the raw otel configs
+  # back to the repository.
+  if [ "$enable_otel_config_write_back" = true ]; then
+    echo "Writing back raw otel configs."
+    write_back
+  fi
+
+  echo "Done."
+}
+
+main
