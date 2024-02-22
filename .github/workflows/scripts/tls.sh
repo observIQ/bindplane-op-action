b69f242ffb5c39dab8ecf003c373713631f6b83a
jsirianni <joe.sirianni@observiq.com>
2024-02-22 14:48:00 -0500

    move step commands to script. set cert permissions

---
+++ .github/workflows/scripts/tls.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -e
+
+wget https://dl.smallstep.com/cli/docs-cli-install/latest/step-cli_amd64.deb
+sudo apt-get install -y -f ./step-cli_amd64.deb
+
+mkdir tls
+
+step certificate create \
+    ca.internal \
+    tls/ca.crt tls/ca.key \
+    --profile root-ca \
+    --no-password \
+    --insecure \
+    --not-after=8760h
+
+step certificate create \
+    bindplane.internal \
+    tls/bindplane.crt tls/bindplane.key \
+    --profile leaf \
+    --not-after 2160h \
+    --no-password \
+    --insecure \
+    --ca tls/ca.crt \
+    --ca-key tls/ca.key
+
+chmod -R 0644 tls
