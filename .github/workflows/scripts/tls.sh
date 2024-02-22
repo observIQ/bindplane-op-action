539bc4ba9c98c71d3cc4e5927c6cd6045f5a085a
jsirianni <joe.sirianni@observiq.com>
2024-02-22 15:41:14 -0500

    debug

--- .github/workflows/scripts/tls.sh
+++ .github/workflows/scripts/tls.sh
@@ -7,31 +7,31 @@ curl -L -s -o step.tar.gz \
 tar -xzf step.tar.gz
 mv step_0.22.0/bin/step /usr/local/bin/step
 
-mkdir /tmp/step
+mkdir step
 
-ls -la /tmp/step
+ls -la step
 
-step certificate create \
+sudo step certificate create \
     ca.internal \
-    /tmp/step/ca.crt /tmp/step/ca.key \
+    step/ca.crt step/ca.key \
     --profile root-ca \
     --no-password \
     --insecure \
     --not-after=8760h
 
-step certificate create \
+sudo step certificate create \
     bindplane.internal \
-    /tmp/step/bindplane.crt /tmp/step/bindplane.key \
+    step/bindplane.crt step/bindplane.key \
     --profile leaf \
     --not-after 2160h \
     --no-password \
     --insecure \
-    --ca /tmp/step/ca.crt \
-    --ca-key /tmp/step/ca.key
+    --ca step/ca.crt \
+    --ca-key step/ca.key
 
-sudo chmod -R 0644 /tmp/step
+sudo chmod -R 0644 step
 
-sudo ls -la /tmp/step
-
-whoami
-echo $USER
+cat step/ca.crt
+cat step/ca.key
+cat step/bindplane.crt
+cat step/bindplane.key
