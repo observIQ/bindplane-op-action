0ea92024c76ef2d80d4738ef9ac4ef59b5e3ef06
Joe Sirianni <joe.sirianni@observiq.com>
2024-05-08 17:30:42 -0400

    configure restry client

--- client/client.go
+++ client/client.go
@@ -2,6 +2,10 @@ package client
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"time"
 
 	"github.com/observiq/bindplane-op-action/client/config"
 	"github.com/observiq/bindplane-op-action/client/version"
@@ -10,16 +14,44 @@ import (
 	"go.uber.org/zap"
 )
 
+const KeyHeader = "X-Bindplane-Api-Key"
+
 type BindPlane struct {
 	logger *zap.Logger
 	config *config.Config
 	client *resty.Client
 }
 
+// NewBindPlane takes a config and logger and returns a configured BindPlane client
 func NewBindPlane(config *config.Config, logger *zap.Logger) (*BindPlane, error) {
+	restryClient := resty.New()
+	restryClient.SetDisableWarn(true)
+	restryClient.SetTimeout(time.Second * 20)
+
+	restryClient.SetBasicAuth(config.Auth.Username, config.Auth.Password)
+
+	if config.Auth.APIKey != "" {
+		restryClient.SetHeader(KeyHeader, config.Auth.APIKey)
+	}
+
+	restryClient.SetBaseURL(fmt.Sprintf("%s/v1", config.Network.RemoteURL))
+
+	tlsConfig := &tls.Config{}
+	if len(config.Network.CertificateAuthority) > 0 {
+		tlsConfig.RootCAs = x509.NewCertPool()
+		for _, ca := range config.Network.CertificateAuthority {
+			if ok := tlsConfig.RootCAs.AppendCertsFromPEM([]byte(ca)); !ok {
+				return nil, fmt.Errorf("failed to append certificate authority")
+			}
+		}
+	}
+
+	restryClient.SetTLSClientConfig(tlsConfig)
+
 	return &BindPlane{
 		logger: logger,
 		config: config,
+		client: restryClient,
 	}, nil
 }
 
