edf0997542b123932fe6ddce6e4786d0b2c91dc3
Joe Sirianni <joe.sirianni@observiq.com>
2024-06-15 13:59:58 -0400

    setup orgs when 1.59.0 or newer

---
+++ .github/workflows/scripts/init.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# This script initializes the BindPlane API with an organization or account
+# depending on the version. Pre 1.59.0 was account based, post 1.59.0 is
+# organization based.
+
+set -e
+
+BINDPLANE_VERSION=$1
+ORGS=false
+
+if [ -z "$BINDPLANE_VERSION" ]; then
+    echo "Usage: $0 <BINDPLANE_VERSION>"
+fi
+
+if [[ $(printf '%s\n' "$BINDPLANE_VERSION" "1.58.0" | sort -V | head -n1) == "1.58.0" && "$BINDPLANE_VERSION" != "1.58.0" ]]; then
+    ORGS=true
+elif [[ "$BINDPLANE_VERSION" == "latest" ]]; then
+    ORGS=true
+fi
+
+if [[ $ORGS == "true" ]]; then
+    curl -v -k \
+        -u admin:admin \
+        https://localhost:3001/v1/organizations \
+        -X POST \
+        -d '{"organizationName": "init", "accountName": "project", "eulaAccepted":true}' \
+        --key step/bindplane.key \
+        --cert step/bindplane.crt
+else
+    curl -v -k \
+        -u admin:admin \
+        https://localhost:3001/v1/accounts \
+        -X POST \
+        -d '{"displayName": "init"}' \
+        --key step/bindplane.key \
+        --cert step/bindplane.crt
+fi
